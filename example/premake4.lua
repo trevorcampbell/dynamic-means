411c9412902acceb727ee18dcd6cd1edd52d546a
trevorcampbell <trevor.d.campbell@gmail.com>
2014-02-06 16:42:18 -0500

    Working on adding spectral dyn means example

--- example/premake4.lua
+++ example/premake4.lua
@@ -1,10 +1,10 @@
-solution "DynMeansExample"
+solution "Examples"
 	configurations{"debug", "release"}
 	project "DynMeansExample"
 		kind "ConsoleApp"
 		language "C++"
 		location "build"
-		files {"main.cpp"}
+		files {"maindm.cpp"}
 		links {"lpsolve55"}
 		includedirs{"/usr/local/include/eigen3", "/usr/local/include/dynmeans"}
 		configuration "debug"
@@ -13,4 +13,17 @@ solution "DynMeansExample"
 		configuration "release"
 			flags{"Optimize"}
 			buildoptions{"-std=c++0x"}
-
+	project "SpecDynMeansExample"
+		kind "ConsoleApp"
+		language "C++"
+		location "build"
+		files {"mainsdm.cpp"}
+		links {"lpsolve55", "gurobi_c++", "gurobi55"}
+		includedirs{"/usr/local/include/eigen3", "/opt/gurobi550/linux64/include", "/usr/local/include/specdynmeans"}
+		libdirs{"/opt/gurobi550/linux64/lib"}
+		configuration "debug"
+			flags{"Symbols", "ExtraWarnings"}
+			buildoptions{"-std=c++0x"}
+		configuration "release"
+			flags{"Optimize"}
+			buildoptions{"-std=c++0x"}
