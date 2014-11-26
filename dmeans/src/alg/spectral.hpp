42ed292e5a31d232df5f3070d9a5909ab5507a15
Trevor Campbell <trevor.d.campbell@gmail.com>
2014-11-26 09:57:13 -0500

    adding spectral algorithm headers

---
+++ dmeans/src/alg/spectral.hpp
@@ -0,0 +1,6 @@
+#ifndef __SPECTRAL_HPP
+
+
+#include "spectral_impl.hpp"
+#define __SPECTRAL_HPP
+#endif /* __SPECTRAL_HPP */
---
+++ dmeans/src/alg/spectral_impl.hpp
@@ -0,0 +1,5 @@
+#ifndef __SPECTRAL_IMPL_HPP
+
+
+#define __SPECTRAL_IMPL_HPP
+#endif /* __SPECTRAL_IMPL_HPP */
